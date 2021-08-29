6f6bc34349ddc33d3e02815b294c7569e35f3c32
princepssol <agarwaen260293@gmail.com>
2021-08-29 16:02:08 +0400

    init maven project

---
+++ src/main/java/com/ecwid/app/App.java
@@ -0,0 +1,13 @@
+package com.ecwid.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
